89eec3327711c52896f6d8efdc1bb04ed56022f8
Jochem Sluijter <126278247+jsluijter02@users.noreply.github.com>
2026-01-14 10:55:31 +0100

    last commit before repo restructuring

--- scripts/utils/get_dirs.py
+++ scripts/utils/get_dirs.py
@@ -1,9 +1,13 @@
-import os
+import os, sys
 
 def get_base_dir():
-    BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
-    assert(os.path.basename(BASE_DIR) == "LightSB_YOLO")
+    BASE_DIR = os.getcwd()
+    while os.path.basename(BASE_DIR) != "LIGHTSB_YOLO":
+        BASE_DIR = os.path.dirname(BASE_DIR)
     return BASE_DIR
+
+def is_colab():
+    return 'google.colab' in sys.modules
 
 if __name__ == "__main__":
     print(get_base_dir())
